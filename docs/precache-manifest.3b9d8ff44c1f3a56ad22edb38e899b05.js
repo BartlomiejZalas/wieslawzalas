5285b8bf1c0d35e1522bad97a93d92fb473e23aa
Bartlomiej Zalas <bartekzalas@gmail.com>
2019-02-03 19:04:34 +0200

    docs added

---
+++ docs/precache-manifest.3b9d8ff44c1f3a56ad22edb38e899b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5bf40a5f529d83f6f2257897cd7507",
+    "url": "/static/media/cover.4e5bf40a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46093684d1c4e9b33a3a",
+    "url": "/static/js/main.46093684.chunk.js"
+  },
+  {
+    "revision": "7d8a00c96b4ab7a68333",
+    "url": "/static/js/1.7d8a00c9.chunk.js"
+  },
+  {
+    "revision": "46093684d1c4e9b33a3a",
+    "url": "/static/css/main.cd3b9f71.chunk.css"
+  },
+  {
+    "revision": "835b93c57b0bf642ed09de270cba1f12",
+    "url": "/index.html"
+  }
+];
