e9fd8950609c5c150fe26eef11b8c83195d147ce
Bartlomiej Zalas <bartekzalas@gmail.com>
2019-02-03 19:11:55 +0200

    homepage propery added

---
+++ docs/precache-manifest.09f41d5cd89706fb02473446c1b285e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5bf40a5f529d83f6f2257897cd7507",
+    "url": "/wieslawzalas/static/media/cover.4e5bf40a.jpg"
+  },
+  {
+    "revision": "d5460464d8d5821ed303",
+    "url": "/wieslawzalas/static/js/runtime~main.d5460464.js"
+  },
+  {
+    "revision": "d32c085c03021f9bceff",
+    "url": "/wieslawzalas/static/js/main.d32c085c.chunk.js"
+  },
+  {
+    "revision": "7d8a00c96b4ab7a68333",
+    "url": "/wieslawzalas/static/js/1.7d8a00c9.chunk.js"
+  },
+  {
+    "revision": "d32c085c03021f9bceff",
+    "url": "/wieslawzalas/static/css/main.cd3b9f71.chunk.css"
+  },
+  {
+    "revision": "d308e2bb9c1205454373e7d93dc3def3",
+    "url": "/wieslawzalas/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b9d8ff44c1f3a56ad22edb38e899b05.js"
+  "/wieslawzalas/precache-manifest.09f41d5cd89706fb02473446c1b285e8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wieslawzalas/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
